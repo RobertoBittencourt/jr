38bfb54674b4f83aa6ae51a34be3d0aafee9a5b7
Roberto A. Bittencourt <robertoab.prof@gmail.com>
2013-09-18 23:47:18 -0300

    feature: added class About3.

---
+++ jabref-2.9.2/src/java/net/sf/jabref/about/About3.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package net.sf.jabref.about;
+
+/**
+ * @author Roberto
+ *
+ */
+public class About3 {
+
+	/**
+	 * 
+	 */
+	public About3() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
